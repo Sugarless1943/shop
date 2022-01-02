2e5f13dd93364e56feef35ed6dff297f49cc62de
yefan <13259459957@163.com>
2022-01-02 17:29:37 +0800

    'service'

---
+++ service-shop/src/test/java/com/yefan/serviceshop/ServiceShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yefan.serviceshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
